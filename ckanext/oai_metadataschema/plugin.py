e580afdf3119f44ed4ee7bca9f0b095b405dffeb
Bhavin Katabathuni <bhavin2897@gmail.com>
2022-04-11 13:57:49 +0200

    Removed related identifier html

--- ckanext/oai_metadataschema/plugin.py
+++ ckanext/oai_metadataschema/plugin.py
@@ -1,10 +1,21 @@
 import ckan.plugins as p
 import ckan.plugins.toolkit as tk
-
+from flask import Blueprint
 
 class OaiMetadataschemaPlugin(p.SingletonPlugin, tk.DefaultDatasetForm):
-    p.implements(p.IDatasetForm)
+    p.implements(p.IDatasetForm, inherit=True)
     p.implements(p.IConfigurer)
+
+    def is_fallback(self):
+        # Return True to register this plugin as the default handler for
+        # package types not handled by any other IDatasetForm plugin.
+        return True
+
+    def package_types(self):
+    #    # This plugin doesn't handle any special package types, it just
+    #    # registers itself as the default (above).
+        return []
+
 
     def create_package_schema(self):
         # let's grab the default schema in our plugin
@@ -37,8 +48,6 @@ class OaiMetadataschemaPlugin(p.SingletonPlugin, tk.DefaultDatasetForm):
                                  tk.get_converter(u'convert_to_extras')],
         })
 
-
-
         return schema
 
     def update_package_schema(self):
@@ -69,8 +78,6 @@ class OaiMetadataschemaPlugin(p.SingletonPlugin, tk.DefaultDatasetForm):
           u'relationType': [tk.get_validator(u'ignore_missing'),
                            tk.get_converter(u'convert_to_extras')],
         })
-
-
         return schema
 
     def show_package_schema(self):
@@ -100,20 +107,15 @@ class OaiMetadataschemaPlugin(p.SingletonPlugin, tk.DefaultDatasetForm):
 
         })
 
-
         return schema
 
-    def is_fallback(self):
-        # Return True to register this plugin as the default handler for
-        # package types not handled by any other IDatasetForm plugin.
-        return True
-
-    def package_types(self):
-        # This plugin doesn't handle any special package types, it just
-        # registers itself as the default (above).
-        return [u'fancy_type']
 
     def update_config(self, config):
         # Add this plugin's templates dir to CKAN's extra_template_paths, so
         # that CKAN will use this plugin's custom templates.
         tk.add_template_directory(config, 'templates')
+        tk.add_public_directory(config, 'public')
+        tk.add_resource('public/statics', 'ckanext-oai-metadataschema')
+
+
+
