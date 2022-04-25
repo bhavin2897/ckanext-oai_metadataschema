5cb13554b02d8432a98e532435fb2cc96d118c1e
Bhavin Katabathuni <bhavin2897@gmail.com>
2022-04-25 16:28:53 +0200

    changing it back

--- ckanext/oai_metadataschema/plugin.py
+++ ckanext/oai_metadataschema/plugin.py
@@ -6,17 +6,6 @@ class OaiMetadataschemaPlugin(p.SingletonPlugin, tk.DefaultDatasetForm):
     p.implements(p.IDatasetForm, inherit=True)
     p.implements(p.IConfigurer)
 
-    def is_fallback(self):
-        # Return True to register this plugin as the default handler for
-        # package types not handled by any other IDatasetForm plugin.
-        return True
-
-    def package_types(self):
-    #    # This plugin doesn't handle any special package types, it just
-    #    # registers itself as the default (above).
-        return []
-
-
     def create_package_schema(self):
         # let's grab the default schema in our plugin
         schema = super(OaiMetadataschemaPlugin, self).create_package_schema()
@@ -108,14 +97,22 @@ class OaiMetadataschemaPlugin(p.SingletonPlugin, tk.DefaultDatasetForm):
         })
 
         return schema
+        
+    def is_fallback(self):
+        # Return True to register this plugin as the default handler for
+        # package types not handled by any other IDatasetForm plugin.
+        return True
 
+    def package_types(self):
+      # This plugin doesn't handle any special package types, it just
+      # registers itself as the default (above).
+        return []
 
     def update_config(self, config):
         # Add this plugin's templates dir to CKAN's extra_template_paths, so
         # that CKAN will use this plugin's custom templates.
         tk.add_template_directory(config, 'templates')
-        tk.add_public_directory(config, 'public')
-        tk.add_resource('public/statics', 'ckanext-oai-metadataschema')
+       
 
 
 
